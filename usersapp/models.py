1744821a5f585a53d8d74671ba0545cdbf0e4e22
Vyachs <KartashovVD@yandex.ru>
2022-09-20 23:03:34 +0700

    lesson_1 Done

--- usersapp/models.py
+++ usersapp/models.py
@@ -1,8 +1,8 @@
-from django.db import models
-from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
@@ -14,9 +14,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         unique=True,
         help_text=_("Required. 80 characters or fewer. Letters, digits and @/./+/-/_ only."),
         validators=[username_validator],
-        error_messages={
-            "unique": _("A user with that username already exists.")
-        },
+        error_messages={"unique": _("A user with that username already exists.")},
     )
     first_name = models.CharField(_("first name"), max_length=100, blank=True)
     last_name = models.CharField(_("last name"), max_length=100, blank=True)
@@ -24,12 +22,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         _("email address"),
         max_length=300,
         unique=True,
-        error_messages={
-            "unique": _("A user with that email already exists.")
-        },
+        error_messages={"unique": _("A user with that email already exists.")},
+    )
+    is_staff = models.BooleanField(
+        _("staff_status"), default=False, help_text=_("Designate whether the user can lig into this admin site.")
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. \
+                Unselect this instead of deleting accounts."
+        ),
     )
     date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
-    date_of_birth = models.DateField(_('date of birth'))
+    date_of_birth = models.DateField(_("date of birth"), null=True)
 
     objects = UserManager()
 
